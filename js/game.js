e7c5a2a75febb3a9ad7c8d52aab7bc228f1f6f62
Shannon Russell <s.russell3271@gmail.com>
2019-03-15 18:02:06 +0000

    adding

--- js/game.js
+++ js/game.js
@@ -21,7 +21,7 @@ var game = {
 
         righthouse: {
             background_image: "righthousehallway.png",
-            message: "You choose to go right and you see a nice fancy house.You wonder if you shoud take a peak and go inside.You see on both sides doors. The first two doors at the front of hallway sticks out to you. One is on the left the other is on the right",
+            message: "You choose to go right and you see a nice fancy house.You wonder if you shoud take a peek and go inside.You see on both sides doors. The first two doors at the front of hallway sticks out to you. One is on the left the other is on the right",
             choices: [
                 {
                     text: "Left Door",
@@ -44,8 +44,8 @@ var game = {
             ]
         },
         leftdoor: {
-                message: "You don't see anything inside so you go back out and go to the other door",
-                choices: [
+            message: "You don't see anything inside so you go back out and go to the other door",            
+            choices: [
                     {
                     text: "Enter the Other Room",
                     nextLevel: "rightdoor",
@@ -53,23 +53,99 @@ var game = {
                 ]
              },
         rightdoor: {
-                message: "You see a raven sitting on the window and around it's neck is a key and a note",
-                choices: [
-                    {
-                    text: "Take the key and note",
-                    nextLevel: "keyFromBird",
-                    },
-                    
-                    {
-                    text: "Leave the Room",
-                    nextLevel: "hallway",
-                    },
-                ]
-        }
- 
-    
-    
-    
+            message: "You see a raven sitting on the window and around it's neck is a key and a note",
+            choices: [
+                {
+                text: "Take the key and note",
+                nextLevel: "afterBird",
+                },
+                
+                {
+                text: "Leave the Room",
+                 nextLevel: "hallway",
+                },
+            ]
+        },
+        afterBird: {
+            message: "The notes says: You found the key now find your friend...Time is ticking so I'll hurry up if I was you. So you either",
+            choices: [
+                {
+                text: "Look For You Friend",
+                nextLevel: "lookForFriend",
+                },
+                
+                {
+                text: "Save Yourself",
+                 nextLevel: "saveYourself",
+                },
+            ]
+        },
+        hallway: {
+            message: "You walked back to the front door and you see something move in the shadows. Then it moves towards you, so you",
+            choices: [
+                {
+                text: "Panic and Run out the house",
+                nextLevel: "leaveRightHouse",
+                },
+                
+                {
+                text: "Stay Calm and Pretend you don't see it.",
+                 nextLevel: "calmNoSee",
+                },
+            ]
+        },
+        leaveRightHouse: {
+            message: "You ran as fast as you could and as far as you can. However, you wasn't no match for your killer. You didn't save your friend and you died.",
+            choices: [
+                {
+                text: "Start Over",
+                nextLevel: "start",
+                },
+            ]
+        },
+               
+        lookForFriend: {
+            message: "You heard screaming from the wall. You walked over it and said 'Anyone here?' A voice from the wall that was oddly familiar said 'Help me~'",
+            choices: [
+                {
+                text: "Look For A Way Through The Wall ",
+                nextLevel: "findingFriend",
+                },
+                
+                {
+                text: "Leave Them Behind",
+                nextLevel: "hallway",
+                },
+            ]
+        },
+        findingFriend: {
+            message: "You see this hook hanging from the wall and you",
+            choices: [
+                {
+                text: "Touched It ",
+                nextLevel: "hook",
+                },
+                
+                {
+                text: "Leave It Alone",
+                nextLevel: "noHook",
+                },
+            ]
+        },
+        hook: {
+            message: "You pulled on it and the wall opened up. You see your friend and she runs to you and grabbed your hand. They want to leave so you",
+            choices: [
+                {
+                text: "Let Her Lead ",
+                nextLevel: "theyEscaping",
+                },
+                
+                {
+                text: "Lead Them Instead",
+                nextLevel: "youEscaping",
+                },
+            ]
+        }    
     
     }
 };
